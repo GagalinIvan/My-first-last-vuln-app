10a0c18d45e1b90dacd200fe3903cee9d06c5e58
Ivan Antonov <ivanantonovjobs@gmail.com>
2022-12-15 16:56:38 +0000

    Update connect.php

--- onlybro/vendor/connect.php
+++ onlybro/vendor/connect.php
@@ -1,8 +1,7 @@
-
 <?php
 ini_set('display_errors',1);
 error_reporting(E_ALL ^E_NOTICE);
-$connect = mysqli_connect('mariadb', 'root', '2101', 'users', 3306);
+$connect = mysqli_connect('mariadb', 'root', '1234', 'users', 3306);
 if(!$connect){
     die("Error");
 }
